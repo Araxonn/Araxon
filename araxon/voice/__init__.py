6f168485e423696946024f97687a2aaf0809d71f
Lokesh <luckypatil577r@gmail.com>
2026-05-19 02:49:33 +0530

    Add voice module initialization and configuration settings

--- araxon/voice/__init__.py
+++ araxon/voice/__init__.py
@@ -1,1 +1,17 @@
 """Voice module for ARAXON."""
+
+from .audio_player import AudioPlayer
+from .listener import MicrophoneListener
+from .synthesizer import KokoroSynthesizer
+from .transcriber import WhisperTranscriber
+from .voice_output import VoiceOutputPipeline
+from .voice_input import VoiceInputPipeline
+
+__all__ = [
+	"VoiceInputPipeline",
+	"VoiceOutputPipeline",
+	"MicrophoneListener",
+	"WhisperTranscriber",
+	"KokoroSynthesizer",
+	"AudioPlayer",
+]
