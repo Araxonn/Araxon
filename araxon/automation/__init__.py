efd5641f6dfce4c7390c4d10e1bcec20f0915076
Lokesh <luckypatil577r@gmail.com>
2026-05-25 01:13:18 +0530

    refactor: organize imports and define __all__ in automation module

--- araxon/automation/__init__.py
+++ araxon/automation/__init__.py
@@ -1,1 +1,17 @@
 """Automation module for ARAXON."""
+
+from .app_launcher import AppLauncher
+from .automation_router import AutomationRouter
+from .browser_agent import BrowserAgent
+from .command_runner import CommandRunner
+from .web_launcher import WebLauncher
+from .workspace_manager import WorkspaceManager
+
+__all__ = [
+	"AutomationRouter",
+	"AppLauncher",
+	"WebLauncher",
+	"BrowserAgent",
+	"CommandRunner",
+	"WorkspaceManager",
+]
