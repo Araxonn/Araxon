80f31f8a3ef50d78947d2d8f6dfedda63ec504c3
Lokesh <luckypatil577r@gmail.com>
2026-05-28 23:58:31 +0530

    update internet module documentation and structure for clarity

--- araxon/internet/__init__.py
+++ araxon/internet/__init__.py
@@ -1,1 +1,17 @@
-"""Internet module for ARAXON."""
+"""ARAXON Internet Intelligence system."""
+
+from araxon.internet.searcher import WebSearcher
+from araxon.internet.extractor import ContentExtractor
+from araxon.internet.researcher import WebResearcher
+from araxon.internet.news_fetcher import NewsFetcher
+from araxon.internet.wiki_lookup import WikiLookup
+from araxon.internet.internet_router import InternetRouter
+
+__all__ = [
+	"WebSearcher",
+	"ContentExtractor",
+	"WebResearcher",
+	"NewsFetcher",
+	"WikiLookup",
+	"InternetRouter",
+]
