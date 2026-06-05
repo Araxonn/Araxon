a3f2d0be61c37c41ff3058c037e172d06b0e1e16
Lokesh <luckypatil577r@gmail.com>
2026-06-06 02:09:19 +0530

    update UI module documentation and import WebSocket components for frontend communication

--- araxon/ui/__init__.py
+++ araxon/ui/__init__.py
@@ -1,1 +1,6 @@
-"""UI module for ARAXON."""
+"""ARAXON UI System - WebSocket communication with frontend."""
+
+from araxon.ui.websocket_server import ARAxonWebSocketServer
+from araxon.ui.ui_bridge import UIBridge
+
+__all__ = ["ARAxonWebSocketServer", "UIBridge"]
