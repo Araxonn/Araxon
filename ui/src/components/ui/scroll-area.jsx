c4053a615fa2575d36d6c0afc51c2380f7b62c27
Lokesh <luckypatil577r@gmail.com>
2026-07-15 10:05:27 +0530

    add ScrollArea component; implement viewport and scrollbar functionality with Radix UI primitives

---
+++ ui/src/components/ui/scroll-area.jsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
+import { cn } from "@/lib/utils"
+
+const ScrollArea = React.forwardRef(
+  ({ className, children, ...props }, ref) => (
+    <ScrollAreaPrimitive.Root>
+      <ScrollAreaPrimitive.Viewport
+        ref={ref}
+        className={cn("h-full w-full rounded-[inherit]", className)}
+        {...props}
+      >
+        {children}
+      </ScrollAreaPrimitive.Viewport>
+      <ScrollAreaPrimitive.Scrollbar
+        orientation="vertical"
+        className="flex touch-none select-none transition-colors"
+      >
+        <ScrollAreaPrimitive.Thumb className="relative flex-1 rounded-full bg-border" />
+      </ScrollAreaPrimitive.Scrollbar>
+    </ScrollAreaPrimitive.Root>
+  )
+)
+ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
+
+export { ScrollArea }
